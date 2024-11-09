954df617545b5e983882cb91aa6970fd3db66025
Natasha041 <163377788+Natasha041@users.noreply.github.com>
2024-11-09 23:55:22 +0530

    Update OSlab6.c

--- OSlab6.c
+++ OSlab6.c
@@ -1,61 +1,54 @@
-#include <pthread.h>
-#include <semaphore.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdlib.h>
+#include<unistd.h>
+#inslude<semaphore.h>
+#include<pthread.h>
 
-sem_t emptySlots, filledSlots, mutex;
-int buffer[5];
-int insertIndex = 0, removeIndex = 0;
+sem_t e,f,s;
+int data[5] , in=0 , out=0 ;
 
-void* producer(void* arg) {
-   sem_wait(&emptySlots);         // Wait for empty slot
-   sem_wait(&mutex);               // Enter critical section
-   int product = rand();           // Generate product
-   buffer[insertIndex] = product;  // Insert product into buffer
-   printf("\nProduct %d produced by thread %lu\n", product, pthread_self());
-   insertIndex = (insertIndex + 1) % 5;
-   sem_post(&mutex);               // Exit critical section
-   sem_post(&filledSlots);         // Signal filled slot
-   sleep(1);
-   return NULL;
+void *producer(void *args)
+{
+   sem_wait(&e);
+   sem_wait(&s);
+   data[in]=rand();
+   in=(in+1)%5;
+   sem_post(&s);
+   sem_post(&f);
+   printf("Producer Executed \n");
 }
 
-void* consumer(void* arg) {
-  for (int i = 0; i < 5; i++) {    // Consume exactly 5 products
-     sem_wait(&filledSlots);       // Wait for filled slot
-     sem_wait(&mutex);             // Enter critical section
-     int product = buffer[removeIndex];  // Remove product from buffer
-     printf("\nProduct %d consumed by thread %lu\n", product, pthread_self());
-     removeIndex = (removeIndex + 1) % 5;
-     sem_post(&mutex);             // Exit critical section
-     sem_post(&emptySlots);        // Signal empty slot
-     sleep(1);
-  }
-  return NULL;
+void *consumer(void *args)
+{
+ int val;
+ do
+	{
+		sem_wait(&f);
+		sem_wait(&s);
+		val=data[out];
+		printf("Consumer Executed \n ");
+		out=(out+1)%5;
+		sem_post(&s);
+		sem_post(&e);
+		sem_getvalue(&e,&val);
+	}While(val!=5);
 }
 
-int main() {
-   pthread_t producerThreads[5], consumerThread;
-
-   sem_init(&filledSlots, 0, 0);   // Initialize filled slots to 0
-   sem_init(&emptySlots, 0, 5);    // Initialize empty slots to buffer size
-   sem_init(&mutex, 0, 1);         // Initialize mutex
-
-   for (int i = 0; i < 5; i++) {
-      pthread_create(&producerThreads[i], NULL, producer, NULL);
-   }
-
-   pthread_create(&consumerThread, NULL, consumer, NULL);
-
-   for (int i = 0; i < 5; i++) {
-      pthread_join(producerThreads[i], NULL);
-   }
-   pthread_join(consumerThread, NULL);
-
-   sem_destroy(&filledSlots);
-   sem_destroy(&emptySlots);
-   sem_destroy(&mutex);
-
-   return 0;
+void main()
+{
+	ptherad_t tid[10],c;
+	int i,j;
+	sem_init(&f,0,0);
+	sem_init(&e,0,5);
+	sem_init(&s,0,1);
+	
+	pthread_create(&c,NULL,consumer,NULL);
+	for(i=0;i<5;i++)
+	{
+		pthread_create(&tid[i],NULL,producer,NULL);
+	}
+	pthraed_join(&c,NULL);
+	for(i=0;i<5;i++)
+	{
+		pthraed_join(&tid[i],NULL);
+	}
 }
